6e547e9d4eb7c867079befb790dc3a7d9c3d316a
Matt Compton <matt@getcryst.al>
2023-02-24 01:13:55 -0500

    switch wallpaper

--- dwm/config.h
+++ dwm/config.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#d65b1f";
+static const char col_cyan[]        = "#9f5435";//"#d65b1f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
